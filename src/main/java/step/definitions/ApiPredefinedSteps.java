e98e5489257467716dfae370d11cdac7f7e8b5fc
Dewan Nadia Sultana <nadiaamin_r@yahoo.com>
2016-12-10 21:25:55 -0500

    add feature template and step definition class

---
+++ src/main/java/step/definitions/ApiPredefinedSteps.java
@@ -0,0 +1,82 @@
+package step.definitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ApiPredefinedSteps {
+	
+	@Given("^My api based URL is \"([^\"]*)\"$")
+	public void my_api_based_URL_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^Port number is \"([^\"]*)\"$")
+	public void port_number_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^My request method is \"([^\"]*)\"$")
+	public void my_request_method_is(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I set header \"([^\"]*)\" as \"([^\"]*)\"$")
+	public void i_set_header_as(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I set query parameter \"([^\"]*)\" as \"([^\"]*)\"$")
+	public void i_set_query_parameter_as(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I set path parameter \"([^\"]*)\" as \"([^\"]*)\"$")
+	public void i_set_path_parameter_as(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^i set request body from \"([^\"]*)\"$")
+	public void i_set_request_body_from(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^I make a request to \"([^\"]*)\"$")
+	public void i_make_a_request_to(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I expect status code as \"([^\"]*)\"$")
+	public void i_expect_status_code_as(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I want to print respones body$")
+	public void i_want_to_print_respones_body() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I expect \"([^\"]*)\" equals \"([^\"]*)\" in response body$")
+	public void i_expect_equals_in_response_body(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^I expect \"([^\"]*)\" contains \"([^\"]*)\" in response body$")
+	public void i_expect_contains_in_response_body(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
